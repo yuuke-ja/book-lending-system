da554b8f8997ec96d33e775117c9ad4f17a37c40
中尾優介 <youjiezhongwei2@gmail.com>
2026-01-29 13:23:37 +0900

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import "server-only";
-import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
