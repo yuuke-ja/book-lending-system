b46243eef99994b2a7147038a85c692b7ff58200
中尾優介 <youjiezhongwei2@gmail.com>
2026-04-21 13:21:49 +0900

    スマホログインエラー仮修正

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,7 +7,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-      authorization: { params: { prompt: "select_account" } },
+      authorization: {
+        params: {
+          prompt: "select_account",
+          hd: "*",
+        },
+      },
     }),
   ],
   callbacks: {
