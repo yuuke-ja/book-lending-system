e0910a6a59dd3473995ddc50153d151e9b597b6a
中尾優介 <youjiezhongwei2@gmail.com>
2026-04-21 15:03:26 +0900

    選択

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,7 +7,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-      authorization: { params: { prompt: "select_account" } },
     }),
   ],
   callbacks: {
